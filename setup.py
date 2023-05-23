0d52aee98a6377e1536e811736451fdff034ada7
Nathan D Olson <nolson@nist.gov>
2023-05-23 08:42:44 -0400

    adding details and entry point

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="seqsleuth",
-    version="0.1.0",
-    description="Metadata Extraction from FASTQ files",
-    author="ND Olson",
-    author_email="nolson@nist.gov",
+    name='seqsleuth',
+    version='0.1.0',
+    description='A sequencing technology metadata extraction tool.',
+    author='ND Olson',
+    author_email='nolson@nist.gov',
+    url='http://github.com/nate-d-olson/seqsleuth',
     packages=find_packages(),
     install_requires=[
-        "pysam",  # Add other dependencies here
+        "pysam"
+    ],
+    entry_points={
+        'console_scripts': [
+            'seqsleuth=seqsleuth.main:__main__',  # replace this with your actual module and function
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
