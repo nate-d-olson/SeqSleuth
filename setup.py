bb65386fbece82a282cdc7ae655a015da664053d
Nathan D Olson <nolson@nist.gov>
2023-06-24 15:38:25 -0400

    bumping version for multi-file type extracting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="seqsleuth",
-    version="0.1.0",
+    version="0.2.0",
     description="A sequencing technology metadata extraction tool.",
     author="ND Olson",
     author_email="nolson@nist.gov",
